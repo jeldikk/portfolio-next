06137fe38fec5f14cbd47057832c87b2f29ecaf1
jeldikk <jeldi.kamal2013@gmail.com>
2022-12-29 08:55:06 +0530

    use react-icons instead of bootstrap-icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout/layout.component";
