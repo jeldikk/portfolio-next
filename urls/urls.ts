29f23502c5b182a1f91e183060bf5488ef1ddfa6
jeldikk <jeldi.kamal2013@gmail.com>
2023-03-12 16:05:30 +0530

    add HOST_NAME env variable and update accordingly in vercel too

--- urls/urls.ts
+++ urls/urls.ts
@@ -1,7 +1,3 @@
 export function createProjectsUrl() {
-  if (process.env.NODE_ENV === "development") {
-    return "http://localhost:3000/api/projects";
-  } else {
-    return "/api/projects";
-  }
+  return `${process.env.HOST_NAME}/api/projects`;
 }
